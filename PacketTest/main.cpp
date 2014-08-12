f39fa7c36c05d169d7a76bd350f730d535ff349f
li <li.chao@li.fedora.com>
2014-08-12 21:29:44 +0800

    包校验工具

--- PacketTest/main.cpp
+++ PacketTest/main.cpp
@@ -12,7 +12,7 @@
 #include "../common/packet.h"
 
 #define TARGET_IP "192.168.1.8"
-#define TARGET_PORT 3030
+#define TARGET_PORT 3040
 
 pthread_mutex_t deadlock;
 
@@ -30,12 +30,32 @@ int main(int argc, char** argv)
 	int idx = 1;
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	int ret = connect(sock, (sockaddr*)&servAddr, uiSocketLen);
-	if(ret == -1)
+	if(ret == 0)
+	{
+		printf("connect success\n");
+	}
+	else
 	{
 		printf("connect error\n");
 		return 1;
 	}
 
+	TestProtocol cTestProtocol;
+	cTestProtocol.m_uiTestDataA = 10;
+	cTestProtocol.m_uiTestDataB = 20;
+	cTestProtocol.m_phPrtcolHead.m_uiIdentifyCode = IDENTIFY_CODE;
+	cTestProtocol.m_phPrtcolHead.m_uiPacketLength = sizeof(cTestProtocol);
+	cTestProtocol.m_usEndCode = END_CODE;
+	TestProtocolUn cTestProtocolUn;
+	cTestProtocolUn.m_tplTestProtocol = cTestProtocol;
+	printf("packet len = %u\n", *(unsigned int*)(cTestProtocolUn.m_szaPacketBuff + 8));
+	printf("testdataA = %u\n", *(unsigned int*)(cTestProtocolUn.m_szaPacketBuff + 12));
+	printf("testdataB = %u\n", *(unsigned int*)(cTestProtocolUn.m_szaPacketBuff + 16));
+
+	while(1)
+	{
+		send(sock, cTestProtocolUn.m_szaPacketBuff, cTestProtocol.m_phPrtcolHead.m_uiPacketLength, MSG_NOSIGNAL);
+	}
 	
 	pthread_mutex_lock(&deadlock);
 	pthread_mutex_lock(&deadlock);
